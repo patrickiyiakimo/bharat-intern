fc82d810b84a76fe4cdff268a35d6cfcffb6b164
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-17 17:24:21 +0100

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,10 @@ import AboutText from "./AboutText";
 const About = () => {
   return (
     <div className="bg-gray-900 pb-20">
-      <div className="ml-20 text-2xl font-mono font-semibold mb-10 text-white pt-20">
+      <div className="ml-20 text-2xl font-mono font-semibold mb-5 text-white pt-20">
         About us
       </div>
-      <p className="text-white ml-20 mb-10 font-mono flex">
+      <p className="text-white ml-20 mb-5 font-mono flex">
         At Bharat Intern, we are passionate about empowering innovations that
         <br />
         shape a better future for businesses and individuals alike.
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import main_image from "../Bharatintern images/service-left-main-pic-ac969d4475c0c5d184cf467d3321d7a2.png";
+import { TbBracketsAngle } from "react-icons/tb";
+import { IoIosPhonePortrait } from "react-icons/io";
 
 const Main = () => {
   return (
-    <div className="pt-20 bg-gray-900 font-mono">
+    <div className="pt-20 pb-20 bg-gray-900 font-mono">
       <p className="text-3xl font-semibold text-white text-right pr-32 mb-10">
         Get Specialised In These Domains
       </p>
@@ -12,8 +14,30 @@ const Main = () => {
         life.
       </p>
       <p className="text-white text-right pr-96">Our skills include:</p>
-      <div>
-        <img src={main_image} alt="programming" className="pl-40" />
+      <div className="flex">
+        <img src={main_image} alt="programming" className="pl-10 pr-12 " />
+        <button className="hover:bg-green-700 cursor-default mt-10 pt-5 text-white font-bold  bg-gray-800  rounded p-3 w-40">
+          <p>
+            <TbBracketsAngle className="w-32 h-10" />
+            Web Development Basics
+          </p>
+        </button>
+        <button className="hover:bg-green-700  cursor-default mt-10 pt-5 block text-white font-bold  bg-gray-500  rounded p-3 w-40">
+          <TbBracketsAngle className="w-32 h-10" />
+          <p>Full Stack Web Development</p>
+        </button>
+        <button className="hover:bg-green-700  cursor-default mt-10 pt-5 block text-white font-bold bg-gray-800  rounded p-3 w-40">
+          <IoIosPhonePortrait className="w-32 h-10" />
+          <p>Andriod Development</p>
+        </button>
+        <button className="hover:bg-green-700  cursor-default mt-10 pt-5 block text-white font-bold bg-gray-500  rounded p-3 w-40">
+          <TbBracketsAngle className="w-32 h-10" />
+          <p>Machine Learning </p>
+        </button>
+        <button className="hover:bg-green-700  cursor-default mt-10 pr-10 pt-5 text-white font-bold bg-gray-800  rounded p-3 w-40">
+          <TbBracketsAngle className="w-32 h-10" />
+          <p>Data Science</p>
+        </button>
       </div>
     </div>
   );
