62432ca93668e359f2494ede0d4417551c5359a8
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-17 12:01:44 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,21 @@ import Bharat_logo from "../Bharatintern images/download.png";
 
 const Navbar = () => {
   return (
-    <div className="flex pl-60 font-semibold bg-white text-blue-900 space-x-10 px-20 pt-5 pb-5 space-x-6 sticky top-0 z-10 w-full">
-      <img src={Bharat_logo} alt="Bharat logo" className="w-28 mr-96" />
-      <p className="mt-3">Home</p>
-      <p className="mt-3">Verify</p>
-      <p className="mt-3">About</p>
-      <p className="mt-3">Contact</p>
-      <button className=" border-dashed border-2 border-green-200 bg-transparent text-black py-2 rounded w-28 hover:bg-green-400 hover:text-white ">
-        Apply
-      </button>
+    <div className="sticky top-0 z-10 w-full">
+      <div className="flex pl-60 shadow-lg  font-semibold bg-white text-blue-900 space-x-10 px-20 pt-5 pb-5 space-x-6  top-0  w-full">
+        <img src={Bharat_logo} alt="Bharat logo" className="w-28 mr-96" />
+        <p className="mt-3">Home</p>
+        <p className="mt-3">Verify</p>
+        <p className="mt-3">About</p>
+        <p className="mt-3">Contact</p>
+        <button className=" border-dashed border-2 border-green-200 bg-transparent text-black py-2 rounded w-28 hover:bg-green-400 hover:text-white ">
+          Apply
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
+
+
