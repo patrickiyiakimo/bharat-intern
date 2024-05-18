d4389d6ee7009641e3882db438dd05f327299054
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-18 17:13:08 +0100

    Update Apply.jsx

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -1,29 +1,24 @@
 import React from "react";
+import image_one from "../Bharatintern images/wd-02b6a38300b1e58689d0ee466bd73119.png";
+import image_two from "../Bharatintern images/ml-75c4224683a93131a31841c29761e808.png";
+import image_three from "../Bharatintern images/ds-52dc057b9c881e2de872abc999eee8c7.png";
+import image_four from "../Bharatintern images/ad-84506a6c9d40392c7d743a2a4e4f85e4.png";
+import image_five from "../Bharatintern images/fs-e46224840cdf6333df6a3709ad781138.png";
+
 
 const Apply = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
-      quibusdam quae sequi perspiciatis maxime architecto. Debitis consequuntur
-      architecto possimus? Reprehenderit, enim ducimus, quam consectetur ipsa
-      harum adipisci iusto labore dolor aliquam recusandae in soluta maxime?
-      Quam necessitatibus mollitia quo sint, est rerum velit explicabo unde?
-      Quasi, fuga. Impedit voluptatem, dolorum ex sapiente possimus repudiandae
-      quam! Maxime adipisci voluptatum quasi a voluptatem dicta sed officia
-      exercitationem, cupiditate magni. Veniam esse ipsum delectus tempore
-      laudantium qui iure doloremque laborum quod id. At neque, quos
-      perspiciatis numquam quo quisquam praesentium animi iure natus obcaecati
-      illo voluptates labore assumenda rerum, doloribus nemo, alias corporis
-      similique velit itaque! Beatae quibusdam eos amet animi molestias illum
-      atque distinctio. Sit voluptatum adipisci quam maxime sint a quidem, esse,
-      dolores ratione minus, veniam eveniet expedita nam fuga iste repudiandae?
-      Fuga eaque aut qui, quaerat dicta accusantium, repellendus placeat atque
-      corporis cumque veniam illum? Harum, ratione ducimus. Tempora iure animi
-      saepe atque est vel amet, laboriosam accusamus temporibus facere. Ex
-      reiciendis corporis quisquam doloribus ab praesentium ipsum expedita
-      assumenda, suscipit, hic, iure et ea similique. Quis nisi ipsam a vel
-      dolore soluta laboriosam fugiat, enim esse totam sunt omnis debitis
-      nostrum dicta. Tenetur, aperiam! Cumque eaque quam iusto at.
+    <div className="bg-gray-900 text-white">
+      <div className="pt-40 text-center text-2xl  font-semibold font-mono pb-20">
+        <p>INTERNSHIPS WE OFFER!</p>
+      </div>
+      <span className="bg-gray-900 grid grid-cols-3 gap-10">
+        <img src={image_one} alt="html" />
+        <img src={image_two} alt="html" />
+        <img src={image_three} alt="html" />
+        <img src={image_four} alt="html" />
+        <img src={image_five} alt="html" />
+      </span>
     </div>
   );
 };
