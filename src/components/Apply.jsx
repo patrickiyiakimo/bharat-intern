045912933452d03c9f6ca998007f36aefa669524
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-22 21:35:53 +0100

    updated

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -4,7 +4,6 @@ import image_two from "../Bharatintern images/ml-75c4224683a93131a31841c29761e80
 import image_three from "../Bharatintern images/ds-52dc057b9c881e2de872abc999eee8c7.png";
 import image_four from "../Bharatintern images/ad-84506a6c9d40392c7d743a2a4e4f85e4.png";
 import image_five from "../Bharatintern images/fs-e46224840cdf6333df6a3709ad781138.png";
-// import { IoArrowForwardCircleSharp } from "react-icons/io5";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
@@ -15,11 +14,13 @@ const Apply = () => {
       <div className="pt-40 text-center text-2xl  font-semibold font-mono pb-20">
         <p>INTERNSHIPS WE OFFER!</p>
       </div>
-      <span className="bg-gray-900  md:grid grid-cols-3 gap-10 md:ml-10 mr-10">
+      <motion.span className="bg-gray-900  md:grid grid-cols-3 gap-10 md:ml-10 mr-10">
         <motion.div
-          initial={{ x: "-100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", delay: 0.5, stiffness: 60 }}
+          whileHover={{
+            scale: 1.1,
+            boxShadow: "0px 0px 8px rgb(0, 128, 120)",
+          }}
+          transition={{ type: "spring", stiffness: 500 }}
           className=" overflow-hidden mb-10 ml-10 mr-10 md:ml-5 pb-10 bg-white rounded-md"
         >
           <img src={image_one} alt="html" className="w-3/4 rounded" />
@@ -35,15 +36,14 @@ const Apply = () => {
             <Link to="/signup" className="ml-24 underline">
               Web Development
             </Link>
-            {/* <button className="ml-20 ">
-              <IoArrowForwardCircleSharp className="" />
-            </button> */}
           </motion.p>
         </motion.div>
         <motion.div
-          initial={{ x: "100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", delay: 0.5, stiffness: 60 }}
+          whileHover={{
+            scale: 1.1,
+            boxShadow: "0px 0px 8px rgb(0, 128, 120)",
+          }}
+          transition={{ type: "spring", stiffness: 500 }}
           className="overflow-hidden mb-10 md:ml-5 bg-white rounded-md pb-10 ml-10 mr-10"
         >
           <img src={image_two} alt="html" className="w-3/4 rounded" />
@@ -59,15 +59,14 @@ const Apply = () => {
             <Link to="/signup" className="ml-24 underline">
               Machine Learning
             </Link>
-            {/* <button className="ml-20">
-              <IoArrowForwardCircleSharp className="" />
-            </button> */}
           </motion.p>
         </motion.div>
         <motion.div
-          initial={{ x: "100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", delay: 0.5, stiffness: 60 }}
+          whileHover={{
+            scale: 1.1,
+            boxShadow: "0px 0px 8px rgb(0, 128, 120)",
+          }}
+          transition={{ type: "spring", stiffness: 500 }}
           className="overflow-hidden mb-10 md:ml-5 bg-white rounded-md pb-10 ml-10 mr-10"
         >
           <img src={image_three} alt="html" className="w-3/4 rounded" />
@@ -83,15 +82,14 @@ const Apply = () => {
             <Link to="/signup" className="ml-28 underline">
               Data Science
             </Link>
-            {/* <button className="ml-20">
-              <IoArrowForwardCircleSharp className="" />
-            </button> */}
           </motion.p>
         </motion.div>
         <motion.div
-          initial={{ x: "100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", delay: 0.5, stiffness: 60 }}
+          whileHover={{
+            scale: 1.1,
+            boxShadow: "0px 0px 8px rgb(0, 128, 120)",
+          }}
+          transition={{ type: "spring", stiffness: 500 }}
           className="overflow-hidden mb-10 md:ml-5 bg-white rounded-md pb-10 ml-10 mr-10"
         >
           <img src={image_four} alt="html" className="w-3/4 rounded" />
@@ -107,22 +105,20 @@ const Apply = () => {
             <Link to="/signup" className="ml-24 underline">
               App Development
             </Link>
-            {/* <button className="ml-20">
-              <IoArrowForwardCircleSharp className="" />
-            </button> */}
           </motion.p>
         </motion.div>
         <motion.div
-          initial={{ x: "100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", delay: 0.5, stiffness: 60 }}
+          whileHover={{
+            scale: 1.1,
+            boxShadow: "0px 0px 8px rgb(0, 128, 120)",
+          }}
+          transition={{ type: "spring", stiffness: 500 }}
           className="overflow-hidden mb-10 md:ml-5 bg-white rounded-md ml-10 mr-10"
         >
           <img src={image_five} alt="html" className="w-3/4 rounded" />
           <motion.p
             whileHover={{
               scale: 1.1,
-              textShadow: "rgb(0, 228, 220)",
               boxShadow: "0px 0px 8px rgb(0, 128, 120)",
             }}
             transition={{ type: "spring", stiffness: 500 }}
@@ -131,12 +127,9 @@ const Apply = () => {
             <Link to="/signup" className="ml-14 underline">
               Full-Stack Web Development
             </Link>
-            {/* <button className="ml-20">
-              <IoArrowForwardCircleSharp className="" />
-            </button> */}
           </motion.p>
         </motion.div>
-      </span>
+      </motion.span>
       <Footer />
     </div>
   );
